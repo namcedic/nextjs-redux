d5c1eb3b29f2d4a658216602a76a6c7b410c41dd
cedric-pc <thanhnamcta52@gmail.com>
2025-06-25 17:13:04 +0700

    feat: paging with list

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 import { StoreProvider } from "@/components/StoreProvider";
 import { Counter } from "@/components/Counter";
+import Test from '@/components/Test'
+import ProductPage from '@/components/Products'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
       {/*<StoreProvider>*/}
         {/*<Counter />*/}
       {/*</StoreProvider>*/}
+      {/*<Test />*/}
+        <ProductPage />
     </main>
   );
 }
