e9ae56e40498dbdffc49b85260a1982ef77736f8
cedric-pc <thanhnamcta52@gmail.com>
2025-07-02 14:55:54 +0700

    fix: add search bar and search func

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import { Pagination, List, Card, notification } from 'antd'
+import { Pagination, List, Card, notification, GetProps, Input } from 'antd'
 import { useSearchParams, useRouter } from 'next/navigation'
 import { useAppDispatch } from '@/lib/redux/hook'
 import { productsRequest } from '@/lib/redux/features/product/slice'
+import Search from 'antd/lib/input/Search'
+import { AudioOutlined } from '@ant-design/icons'
 
 const PAGE_SIZE = 10
 
@@ -40,36 +42,60 @@ const ProductPage = () => {
 		router.push(`?page=${pageNumber}`)
 	}
 
+	const onSearch = (value: string) => {
+		dispatch(
+			productsRequest({
+				page,
+				limit: PAGE_SIZE,
+				key: value,
+				cb: (res) => {
+					if (res.products) {
+						setProducts(res.products)
+						setTotal(res.total || 0)
+					} else {
+						notification.error({ message: res.error || 'Get products failed' })
+					}
+				}
+			})
+		)
+	}
+
 	return (
-		<div style={{ padding: 24 }}>
-			<h2>Danh sách sản phẩm</h2>
+		<>
+			<div className='product-search'>
+				<Search placeholder='input search text' onSearch={onSearch} />{' '}
+			</div>
 
-			<List
-				grid={{
-					gutter: 16,
-					xs: 1,
-					sm: 2,
-					md: 2,
-					lg: 3,
-					xl: 4,
-					xxl: 4
-				}}
-				dataSource={products}
-				renderItem={(item) => (
-					<List.Item>
-						<Card title={item.name}>{item.description}</Card>
-					</List.Item>
-				)}
-			/>
+			<div style={{ padding: 24 }}>
+				<h2>Danh sách sản phẩm</h2>
 
-			<Pagination
-				current={page}
-				pageSize={PAGE_SIZE}
-				total={total}
-				onChange={onChangePage}
-				style={{ marginTop: 16, textAlign: 'center' }}
-			/>
-		</div>
+				<List
+					grid={{
+						gutter: 16,
+						xs: 1,
+						sm: 2,
+						md: 2,
+						lg: 3,
+						xl: 4,
+						xxl: 4
+					}}
+					dataSource={products}
+					renderItem={(item) => (
+						<List.Item>
+							<Card title={item.name}>{item.description}</Card>
+						</List.Item>
+					)}
+				/>
+
+				<Pagination
+					current={page}
+					pageSize={PAGE_SIZE}
+					total={total}
+					onChange={onChangePage}
+					style={{ marginTop: 16, textAlign: 'center' }}
+				/>
+			</div>
+		</>
 	)
 }
 
